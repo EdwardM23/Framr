f86acd7991137bd7c66c299eb5ab8ef4eb09c82b
Christopher Edward <scrith.edward23@gmail.com>
2024-03-20 16:11:32 +0700

    add frame to picked image

--- app/src/main/java/com/edward/framr/MainActivity.kt
+++ app/src/main/java/com/edward/framr/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.edward.framr
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
@@ -13,6 +17,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.edward.framr.databinding.ActivityMainBinding
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.InputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -39,8 +46,13 @@ class MainActivity : AppCompatActivity() {
 
     var activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == RESULT_OK && result.data != null) {
-            binding.imgDisplay.setImageURI(result.data!!.data)
-            Toast.makeText(this, "Pick image succeed", Toast.LENGTH_SHORT).show()
+            val image = result.data!!.data
+            var img_final = image?.let {
+                uriToBitmap(this,
+                    it
+                )?.let { addWhiteFrameToImage(it, 100) }
+            }
+            binding.imgDisplay.setImageBitmap(img_final)
         } else if (result.resultCode == RESULT_CANCELED) {
             Toast.makeText(this, "Pick image canceled", Toast.LENGTH_SHORT).show()
         }
@@ -55,5 +67,51 @@ class MainActivity : AppCompatActivity() {
         activityResultLauncher.launch(pickPhoto)
     }
 
+    fun addWhiteFrameToImage(inputBitmap: Bitmap, frameWidth: Int): Bitmap {
+        val width = inputBitmap.width
+        val height = inputBitmap.height
 
+        // Calculate the dimensions for the result bitmap (including the frame)
+        val resultWidth = width + 2 * frameWidth
+        val resultHeight = height + 2 * frameWidth + 300
+
+        // Create a new bitmap with the dimensions for the result image (including the frame)
+        val resultBitmap = Bitmap.createBitmap(resultWidth, resultHeight, Bitmap.Config.ARGB_8888)
+
+        // Create a canvas using the result bitmap
+        val canvas = Canvas(resultBitmap)
+
+        // Draw the white frame (a rectangle) onto the canvas
+        val framePaint = Paint().apply {
+            color = Color.WHITE
+            style = Paint.Style.FILL // Fill the rectangle with the specified color
+        }
+        canvas.drawRect(0f, 0f, resultWidth.toFloat(), resultHeight.toFloat(), framePaint)
+
+        // Draw the input bitmap onto the canvas, shifted by the frame width
+        canvas.drawBitmap(inputBitmap, frameWidth.toFloat(), frameWidth.toFloat(), null)
+
+        return resultBitmap
+    }
+
+    fun uriToBitmap(context: Context, imageUri: Uri): Bitmap? {
+        var inputStream: InputStream? = null
+        try {
+            // Open an input stream from the image URI
+            inputStream = context.contentResolver.openInputStream(imageUri)
+
+            // Decode the input stream into a Bitmap
+            return BitmapFactory.decodeStream(inputStream)
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } finally {
+            try {
+                // Close the input stream
+                inputStream?.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        return null
+    }
 }
